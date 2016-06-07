90f748a0b4fa9a1ed202a0a83539765e81a2cb72
Benjamin Logue and Julia Dickey <benjamin@>
2016-06-07 10:17:32 -0700

    Create word puzzle game

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,71 @@
 $(document).ready(function(){
 
-  $('#count').submit(function(event){
-    event.preventDefault();
-
-    var numberToCountTo = parseInt($('#countTo').val());
-    var numberToCountBy = parseInt($('#countBy').val());
-    var total = 0;
-    console.log(numberToCountTo);
-    console.log(numberToCountBy);
-
-    if (isNaN(numberToCountBy) || isNaN(numberToCountTo)) {
-      alert("Please enter a number");
-    } else if (numberToCountBy < 0 || numberToCountTo < 0) {
-      alert("Please enter a positive number");
-    } else if (numberToCountBy > numberToCountTo) {
-      alert("Please enter a larger number to count to");
-    } else {
-
-      for (i = numberToCountBy; i <= numberToCountTo; i+=numberToCountBy) {
-          console.log(total += numberToCountBy);
-      }
-    }
 
 
+
+
+  // $('#count').submit(function(event){
+  //   event.preventDefault();
+
+    // NUMBER ADD
+    // var numberToCountTo = parseInt($('#countTo').val());
+    // var numberToCountBy = parseInt($('#countBy').val());
+    // var total = 0;
+    // console.log(numberToCountTo);
+    // console.log(numberToCountBy);
+    //
+    // if (isNaN(numberToCountBy) || isNaN(numberToCountTo)) {
+    //   alert("Please enter a number");
+    // } else if (numberToCountBy < 0 || numberToCountTo < 0) {
+    //   alert("Please enter a positive number");
+    // } else if (numberToCountBy > numberToCountTo) {
+    //   alert("Please enter a larger number to count to");
+    // } else {
+    //
+    //   for (i = numberToCountBy; i <= numberToCountTo; i+=numberToCountBy) {
+    //       console.log(total += numberToCountBy);
+    //   }
+    // }
+
+    // WORD PUZZLE -- HIDING THE VOWELS
+
+    $('#quotePuzzle').submit(function(event){
+      event.preventDefault();
+
+    // var quote = $("#quoteToPuzzle").val().toLowerCase().split([','[String.length]]);
+    // var newQuote = "";
+    // console.log(quote);
+    // for ( i=0; i < quote.length; i++ ){
+    //
+    //   if (quote[i] === "a" || quote[i] === "e" || quote[i] === "i" || quote[i] === "o" || quote[i] === "u" ) {
+    //
+    //       quote[i] = "-";
+    //
+    //   } else {
+    //     false;
+    //   }
+    // }
+    // $(".container").children().toggle();
+    // newQuote = quote.join("");
+    // $("#output").append("<p>" + newQuote + "</p>");
+    //
+    // $("#showForm").click(function(){
+    //   $(".container").children().toggle();
+
+    // PALINDROMES
+
+    $('#palindromeInput').submit(function(event){
+      event.preventDefault();
+
+      
+
+
+
+
+
+
+
+
+    });
   });
 });
